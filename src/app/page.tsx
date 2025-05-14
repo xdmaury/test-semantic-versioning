f5c2b2aa924c35cb6428fb61bafd78d574173e62
Maury <33840821+xdmaury@users.noreply.github.com>
2025-05-14 15:14:46 -0300

    Add description (#7)
    * Add introductory paragraph to Home page
    
    * "Updated version 0.2.7 -> 0.2.8."
    
    * "Updated version 0.2.8 -> 0.3.8-beta."

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ export default function Home() {
   return (
     <div>
       Home page!
+      <p>
+        This is a simple example of a Next.js app with TypeScript.
+      </p>  
     </div>
   );
 }
