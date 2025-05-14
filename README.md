fbbd094122d6953e868409948917b0a70568683f
Maury <33840821+xdmaury@users.noreply.github.com>
2025-05-14 17:02:50 -0300

    test (#18)
    * Update README.md: add explanation about version bumping when creating a branch (#15)
    
    * Update README.md: correct version bumping description from branch to … (#16)
    
    * Update README.md: correct version bumping description from branch to pull request
    
    * "Updated version 0.3.15 -> -dev0.3.16."
    
    * "Updated version -dev0.3.16 -> NaN.3.16."
    
    * Remove 'prefix' property from main branch pull-request rules
    
    * Fix version number in package.json and package-lock.json from NaN.3.16 to 0.3.16
    
    * Add additional paragraph to Home component about Next.js and TypeScript version
    
    * Remove redundant paragraph about Next.js and TypeScript version from … (#17)
    
    * Remove redundant paragraph about Next.js and TypeScript version from Home component
    
    * "Updated version 0.3.16 -> 0.3.17."
    
    * "Updated version 0.3.17 -> 0.4.0."

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## This is a test semantic version
+
+When you create a pull request the version will be bumped
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <p>
         This is a simple example of a Next.js app with TypeScript.
       </p>  
+      <button>Test</button>
     </div>
   );
 }
