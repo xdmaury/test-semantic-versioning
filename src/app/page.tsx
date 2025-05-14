cce3d5333e147a3985f878a1993f283d841ed266
xdmaury <maury1418@hotmail.com>
2025-05-14 16:59:38 -0300

    Remove redundant paragraph about Next.js and TypeScript version from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ export default function Home() {
       <p>
         This is a simple example of a Next.js app with TypeScript.
       </p>  
-      <p>
-        It uses the latest version of Next.js and TypeScript.
-      </p>
+      <button>Test</button>
     </div>
   );
 }
